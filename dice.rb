5f7b5c5736b5508415ce536cc18660fad09739c3
Arsalan Bardsiri <35473718+arsalanbardsiri@users.noreply.github.com>
2025-09-04 04:05:24 +0000

    All dices

--- dice.rb
+++ dice.rb
@@ -15,6 +15,13 @@ get("/") do
   "
 end
 
+=begin
+GET /dice/2/6 (simulate two 6-sided dice)
+GET /dice/2/10 (simulate two 10-sided dice)
+GET /dice/1/20 (simulate one 20-sided die)   
+GET /dice/5/4 (simulate five 4-sided dice)
+=end
+
 get("/dice/2/6") do
   first_dice = rand(1..6)
   second_dice = rand(1..6)
@@ -22,6 +29,39 @@ get("/dice/2/6") do
 
   outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  body = +"<h1>1d20</h1>"
+  body << "<p>#{outcome}</p>"
+  body
+end
+
+get("/dice/2/10") do
+  first_dice = rand(1..10)
+  second_dice = rand(1..10)
+  sum = first_dice + second_dice
+
+  outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
+
+  body = +"<h1>1d20</h1>"
+  body << "<p>#{outcome}</p>"
+  body
+end
+
+get("/dice/1/20") do
+  dice = rand(1..20)
+  outcome = "You rolled a #{dice}"
+
+  body = +"<h1>1d20</h1>"
+  body << "<p>#{outcome}</p>"
+  body
+end
+
+get("/dice/5/4") do
+  dices = []
+  4.times do |dice|
+    dice = rand(1..4)
+    dices.push(dice)
+  end
+  body = +"<h1>1d20</h1>"
+  body << dices.map { |num| "<p>#{num}</p>" }.join
+  body
 end
